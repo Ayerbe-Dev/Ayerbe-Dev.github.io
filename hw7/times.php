9bf44d23ac995b5d24c5b78d610bd3eefe6d8559
Henry Nissen <63805100+Ayerbe-Dev@users.noreply.github.com>
2025-03-28 19:50:00 -0400

    test 3

--- hw7/times.php
+++ hw7/times.php
@@ -4,11 +4,11 @@
     <title>Times Table</title>
 </head>
 <body>
-    <h1>Times Table</h1>
-    <?php
-        $n = $_REQUEST['n'];
-        for($i = 1; $i <= 15; $i++)
-            $p = $i * $n
-            echo "$n x $i = $p<br>";
-    ?>
+<?php
+    $n = $_REQUEST['n'];
+    for($i = 1; $i <= 15; $i++) {
+        $p = $i * $n;
+        echo "$n x $i = $p<br>";
+    }
+?>
 </body>
